2e84a356ec688ee10d4593b2e6530ae6a6be9c5f
Farhan Tahmid <farhantahmid881@gmail.com>
2022-05-16 22:18:00 +0600

    Modified arbitary functions

--- FUNCTIONS/arbitaryNumberOfArguments.py
+++ FUNCTIONS/arbitaryNumberOfArguments.py
@@ -1,6 +1,6 @@
-def makePizza(*toppings):
-    print("Making pizza with following toppings: ")
+def makePizza(size,*toppings):
+    print(f"Making {size} inch pizza with following toppings: ")
     for topping in toppings:
         print(f"- {topping}")
-makePizza('pepperoni')
-makePizza('mushrooms','green peppers','extra cheese')
+makePizza(8,'pepperoni')
+makePizza(16,'mushrooms','green peppers','extra cheese')
