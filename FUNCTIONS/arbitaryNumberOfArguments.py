b8143a5a7f79b17598f9475524ab33189ce469ac
Farhan Tahmid <farhantahmid881@gmail.com>
2022-05-16 22:28:36 +0600

    Arbitrary keyword arguments

--- FUNCTIONS/arbitaryNumberOfArguments.py
+++ FUNCTIONS/arbitaryNumberOfArguments.py
@@ -4,3 +4,15 @@ def makePizza(size,*toppings):
         print(f"- {topping}")
 makePizza(8,'pepperoni')
 makePizza(16,'mushrooms','green peppers','extra cheese')
+
+#Using arbitary keyword arguments
+def buildProfileOfUser(firstName,lastName,**userInfo):
+    userInfo['first_Name']=firstName
+    userInfo['last_name']=lastName
+    return userInfo
+user_profile=buildProfileOfUser('farhan','tahmid',
+                                location='dhaka',university='nsu'
+                                )
+print(user_profile)
+
+
